ea3c62ac1312c5b5c580770179c312286e14d0c7
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2014-01-04 21:25:36 +0100

    unique timestamps

--- scripts/post_locations.sh
+++ scripts/post_locations.sh
@@ -1,4 +1,4 @@
 curl -si -H "Content-type: application/json" -X POST \
-	-d '[{"latitude":47.3765,"longitude":8.5545,"time":1388863832489},{"latitude":47.3764,"longitude":8.5546,"time":1388863832489}]' \
+	-d '[{"latitude":47.3765,"longitude":8.5545,"time":1388863832489},{"latitude":47.3764,"longitude":8.5546,"time":1388863832597}]' \
 	http://localhost:8080/location/list
 

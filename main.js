0af1e73dc3622bfefdf41de1056279e5658d8a10
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2014-01-04 13:09:05 +0100

    use Google Maps API only

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-    var ghyb = new OpenLayers.Layer.Google('Google Hybrid', {
-        type: google.maps.MapTypeId.HYBRID,
-        numZoomLevels: 22,
-        sphericalMercator: true
-    });
-
-    var map = new OpenLayers.Map('map', {
-        projection: new OpenLayers.Projection('EPSG:900913')
-    });
-    map.addControl(new OpenLayers.Control.LayerSwitcher());
-    map.addLayers([ghyb]);
-
-    var map_x = 8.554;
-    var map_y = 47.3765;
-    var map_center = new OpenLayers.LonLat(map_x, map_y).transform(new OpenLayers.Projection('EPSG:4326'), map.getProjectionObject());
-    map.setCenter(map_center, 17);
-
     var coords = [
         [8.553354, 47.376119],
         [8.552711, 47.376840],
@@ -26,33 +9,15 @@ $(document).ready(function() {
         [8.553059, 47.377147],
         [8.554392, 47.376924]
         ];
+    var map_x = 8.554;
+    var map_y = 47.3765;
 
-    var points = [];
-    $.each(coords, function(k, coord) {
-        var point = new OpenLayers.LonLat(coord[0], coord[1]).transform(new OpenLayers.Projection('EPSG:4326'), map.getProjectionObject());
-        points.push(new OpenLayers.Geometry.Point(point.lon, point.lat));
-    });
-    var line = new OpenLayers.Geometry.LineString(points);
-    var style = {
-        strokeColor: '#0000ff',
-        strokeOpacity: 1,
-        strokeWidth: 5
-    };
-    var lineFeature = new OpenLayers.Feature.Vector(line, null, style);
-
-    var vectorLayer = new OpenLayers.Layer.Vector("Lines", {
-        projection: new OpenLayers.Projection('EPSG:900913')
-    });
-    vectorLayer.addFeatures([lineFeature]);
-    map.addLayer(vectorLayer);
-
-    // GOOGLE MAPS
     var mapOptions = {
         center: new google.maps.LatLng(map_y, map_x),
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     };
-    var map2 = new google.maps.Map(document.getElementById("map2"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     var points = [];
     $.each(coords, function(k, coord) {
         points.push(new google.maps.LatLng(coord[1], coord[0]));
@@ -62,7 +27,7 @@ $(document).ready(function() {
         strokeColor: "#0000ff",
         strokeOpacity: 0.5,
         strokeWeight: 5,
-        map: map2
+        map: map
     });
 });
 

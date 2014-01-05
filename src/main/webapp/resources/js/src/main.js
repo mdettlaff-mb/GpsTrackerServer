20dbc014c161cf89bcf37430fb3a47a59e513083
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2014-01-05 01:31:12 +0100

    auto center on route

--- src/main/webapp/resources/js/src/main.js
+++ src/main/webapp/resources/js/src/main.js
@@ -1,23 +1,22 @@
 $(function() {
 
 	var createMap = function() {
-		var mapX = 8.554;
-		var mapY = 47.3765;
-
 		var mapOptions = {
-			center: new google.maps.LatLng(mapY, mapX),
-			zoom: 17,
 			mapTypeId: google.maps.MapTypeId.SATELLITE
 		};
 		return new google.maps.Map(document.getElementById('map'), mapOptions);
 	};
 
 	var drawRoute = function(map, locations) {
+		var limits = new google.maps.LatLngBounds();
 		var points = [];
 		$.each(locations, function(k, location) {
-			points.push(new google.maps.LatLng(location.latitude, location.longitude));
+			var position = new google.maps.LatLng(location.latitude, location.longitude);
+			limits.extend(position);
+			points.push(position);
 		});
-		var polyline = new google.maps.Polyline({
+		map.fitBounds(limits);
+		new google.maps.Polyline({
 			path: points,
 			strokeColor: '#0000ff',
 			strokeOpacity: 0.5,
